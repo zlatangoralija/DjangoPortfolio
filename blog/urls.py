339b0fc4a4fb581d218657f1c899ae43f858c732
zlatan-ubuntu <zlatan@ubuntu.com>
2020-06-30 21:14:09 +0200

    polishing

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allblogs, name='home'),
+    path('', views.allblogs, name='blogs'),
     path('<int:blog_id>/', views.details, name='details')
 ]
